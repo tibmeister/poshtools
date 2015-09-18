83b6dbcbd75830fbf6272f0ef0e130b00cb5db90
Adam Driscoll <adam.r.driscoll@gmail.com>
2015-09-18 11:01:37 -0500

    Initial commit of snippet support

--- PowerShellTools/PowerShellToolsPackage.cs
+++ PowerShellTools/PowerShellToolsPackage.cs
@@ -108,11 +108,11 @@ namespace PowerShellTools
     [ProvideLanguageExtension(typeof(PowerShellLanguageInfo), ".ps1")]
     [ProvideLanguageExtension(typeof(PowerShellLanguageInfo), ".psm1")]
     [ProvideLanguageExtension(typeof(PowerShellLanguageInfo), ".psd1")]
-    [ProvideLanguageCodeExpansion(typeof(PowerShellLanguageInfo),
+    [ProvideLanguageCodeExpansion(GuidList.PowerShellLanguage,
         "PowerShell",        // Name of language used as registry key
         0,                   // Resource ID of localized name of language service
         "PowerShell",        // Name of Language attribute in snippet template
-        @"%TestDocs%\Code Snippets\PowerShel\SnippetsIndex.xml",  // Path to snippets index
+        @"%TestDocs%\Code Snippets\PowerShell\SnippetsIndex.xml",  // Path to snippets index
         SearchPaths = @"%TestDocs%\Code Snippets\PowerShell\")]    // Path to snippets
 
     [ProvideDebugPortSupplier("Powershell Remote Debugging (SSL Required)", typeof(RemoteDebugPortSupplier), PowerShellTools.Common.Constants.PortSupplierId, typeof(RemotePortPicker))]
