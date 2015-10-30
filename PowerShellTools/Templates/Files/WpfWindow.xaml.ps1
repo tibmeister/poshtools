009a9581543becb21d6456a364067038ab65f759
Adam Driscoll <adam.r.driscoll@gmail.com>
2015-10-30 10:42:02 -0500

    Item templates

---
+++ PowerShellTools/Templates/Files/WpfWindow.xaml.ps1
@@ -0,0 +1,13 @@
+﻿try{
+
+ Add-Type -AssemblyName PresentationCore,PresentationFramework,WindowsBase,system.windows.forms
+
+} catch {
+
+ Throw "Failed to load Windows Presentation Framework assemblies."
+
+}
+
+[xml]$Global:xmlWPF = Get-Content -Path .\$itemname$
+$Global:xamGUI = [Windows.Markup.XamlReader]::Load((new-object System.Xml.XmlNodeReader $xmlWPF))
+$xamGUI.ShowDialog()
