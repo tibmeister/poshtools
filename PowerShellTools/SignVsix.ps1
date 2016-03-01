212924ebdb3729a46d8809b73739f253aac03342
Adam Driscoll <adam.r.driscoll@gmail.com>
2016-03-01 16:15:30 -0600

    Implements #415

---
+++ PowerShellTools/SignVsix.ps1
@@ -0,0 +1,7 @@
+﻿param($VSIXPath)
+
+$ToolPath = (Join-Path $PSScriptRoot '..\packages\Microsoft.VSSDK.Vsixsigntool.14.1.24720\tools\vssdk\vsixsigntool.exe')
+
+Set-Location $PSScriptRoot
+"sign /f DigiCertNov2016.pfx /p $Env:PoshTools_VSIX_CertPassword $VSIXPath"
+Start-Process $ToolPath -ArgumentList "sign /f DigiCertNov2016.pfx /sha1 a16105fe72a347d6e5c3bd84e8285fd7c9457ef7 /p $Env:PoshTools_VSIX_CertPassword /t http://timestamp.digicert.com $VSIXPath" -NoNewWindow 
